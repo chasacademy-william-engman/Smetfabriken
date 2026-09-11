76c842760da4908bb442a23cda04dccb597523f3
chasacademy-william-engman <william.engman@student.chasacademy.se>
2026-09-11 11:38:18 +0200

    magnific first site

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function smeta() {
+  const lager = document.getElementById("splash-lager");
 
+  const img = document.createElement("img");
+  img.src = "bilder/smetklick.png";
+  img.alt = "";
+  img.className = "splash";
+  img.style.setProperty("--v", (Math.random() * 60 - 30) + "deg");
+
+  lager.appendChild(img);
+  void img.offsetWidth;
+  img.classList.add("kor");
+
+  img.addEventListener("animationend", () => img.remove());
+}
